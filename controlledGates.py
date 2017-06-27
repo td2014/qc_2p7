f3f087e0d880c1e3ab03f803172b46eba84f9401
td2014 <anthonyd2004@gmail.com>
2017-06-27 09:13:16 -0700

    Fixed up controlled gate logic

--- controlledGates.py
+++ controlledGates.py
@@ -19,7 +19,7 @@ os.environ['PYQUIL_CONFIG'] = '/Users/anthonydaniell/Desktop/FilesToStay/Researc
 # Load pyquil
 import pyquil.quil as pq
 import pyquil.api as api
-from pyquil.gates import *
+from pyquil.gates import X, Y, Z, CNOT
 
 # Start of regular code
 
@@ -49,14 +49,80 @@ def controlled(uMatrix):
     
     return newMatrix
 
-
 # Create test matrix and call function
-testMatrix = np.array([[0,1],[1,0]])
+testMatrix = np.array([[0,-1j],[1j,0]])
 
 cMatrix = controlled(testMatrix)
 
 # Display computed matrix
-            
+print cMatrix  
+print
+print
+ 
+#
+# Apply to given some wavefunctions
+#
+p = pq.Program() # clear existing results
+p = pq.Program(CNOT(0, 1))
+wvf, _ = qvm.wavefunction(p)
+print "CNOT|00> = ", wvf
+print "With outcome probabilities\n", wvf.get_outcome_probs()
+print
+
+p = pq.Program(X(0), CNOT(0, 1))
+wvf, _ = qvm.wavefunction(p)
+print "CNOT|01> = ", wvf
+print "With outcome probabilities\n", wvf.get_outcome_probs()
+print
+
+p = pq.Program(X(1), CNOT(0, 1))
+wvf, _ = qvm.wavefunction(p)
+print "CNOT|10> = ", wvf
+print "With outcome probabilities\n", wvf.get_outcome_probs()
+print
+
+p = pq.Program(X(0), X(1), CNOT(0, 1))
+wvf, _ = qvm.wavefunction(p)
+print "CNOT|11> = ", wvf
+print "With outcome probabilities\n", wvf.get_outcome_probs()
+print
+print   
+#
+# Apply new gate
+#
+print "New Gate"
+print
+p = pq.Program() # clear existing results
+p = pq.Program().defgate(name="NEW_G", matrix=cMatrix)
+p.inst(("NEW_G", 0, 1))
+wvf, _ = qvm.wavefunction(p)
+print "NEW_G|00> = ", wvf
+print "With outcome probabilities\n", wvf.get_outcome_probs()
+print
+
+p = pq.Program().defgate("NEW_G", cMatrix)
+p.inst(X(0))
+p.inst(("NEW_G", 0, 1))
+wvf, _ = qvm.wavefunction(p)
+print "NEW_G|01> = ", wvf
+print "With outcome probabilities\n", wvf.get_outcome_probs()
+print
+
+p = pq.Program().defgate("NEW_G", cMatrix)
+p.inst(X(1))
+p.inst(("NEW_G", 0, 1))
+wvf, _ = qvm.wavefunction(p)
+print "NEW_G|10> = ", wvf
+print "With outcome probabilities\n", wvf.get_outcome_probs()
+print
+
+p = pq.Program().defgate("NEW_G", cMatrix)
+p.inst(X(0), X(1))
+p.inst(("NEW_G", 0, 1))
+wvf, _ = qvm.wavefunction(p)
+print "NEW_G|11> = ", wvf
+print "With outcome probabilities\n", wvf.get_outcome_probs()
+  
 #
 # End of script
 #
